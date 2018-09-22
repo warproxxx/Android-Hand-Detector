14551af080b9fd936290853622a3d9bf7eee2257
Daniel Sapkota <daniel@quantorithm.com>
2018-09-22 17:13:35 +0545

    Initial Commit

---
+++ src/main/java/com/example/warproxxx/tensorflowhanddetector/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.warproxxx.tensorflowhanddetector;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        System.out.println("Test");
+        Bitmap bitmap = BitmapFactory.decodeFile("file:///assets/hand.jpg");
+        Bitmap resizedBitmap = getResizedBitmap(bitmap, 576, 1024);
+        float[] floatArr = a(resizedBitmap);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
+    public static float[] a(Bitmap bitmap) {
+        float[] fArr = new float[((bitmap.getWidth() * bitmap.getHeight()) * 3)];
+        int[] iArr = new int[(bitmap.getWidth() * bitmap.getHeight())];
+        bitmap.getPixels(iArr, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        int i = 0;
+        for (int i2 = 0; i2 < iArr.length; i2++) {
+            fArr[i + 2] = ((float) ((iArr[i2] >> 16) & 255)) / 255.0f;
+            fArr[i + 1] = ((float) ((iArr[i2] >> 8) & 255)) / 255.0f;
+            fArr[i] = ((float) (iArr[i2] & 255)) / 255.0f;
+            i += 3;
+        }
+        return fArr;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
